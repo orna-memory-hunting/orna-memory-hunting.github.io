9a0d919fa2c1bd7ba0ea367af51793e8b35d973c
IgorNovozhilov <maildrakon@gmail.com>
2022-09-15 22:18:18 +0300

    webmanifest \ DEV

--- test.js
+++ test.js
@@ -13,10 +13,19 @@ app
   .get('/', async (req, reply) => {
     const index = (await readFile('./docs/index.html', 'utf-8'))
       .replace(/---\n---\n/, '')
+      .replace(/Memory Hunting - Orna/, 'Memory Hunting - Orna (DEV)')
 
     reply.type('text/html')
 
     return index
+  })
+  .get('/orna-memory-hunting.webmanifest', async (req, reply) => {
+    const manifest = (await readFile('./docs/orna-memory-hunting.webmanifest', 'utf-8'))
+      .replace(/Memory Hunting/g, 'Memory Hunting (DEV)')
+
+    reply.type('application/manifest+json')
+
+    return manifest
   })
   .ready(async () => {
     try {
