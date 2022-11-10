536aa01e0a56c4b99708c1e2daf7bd69cad656ac
IgorNovozhilov <maildrakon@gmail.com>
2022-11-11 00:48:15 +0300

    loadSummaryTable+SearchParams

--- docs/page/summary-table.js
+++ docs/page/summary-table.js
@@ -2,12 +2,16 @@ import { safeExecute } from '../lib/utils.js'
 import { getIssuesMap } from '../lib/github.js'
 
 safeExecute(async () => {
+  const params = new URLSearchParams(window.location.hash.replace('#', ''))
+  const milestone = parseInt(params.get('milestone'))
+
   // TODO
-  await loadSummaryTable()
+  await loadSummaryTable(isNaN(milestone) ? null : milestone)
 })
 
-async function loadSummaryTable() {
-  const issuesMap = await getIssuesMap()
+/** @param {number} milestone */
+async function loadSummaryTable(milestone) {
+  const issuesMap = await getIssuesMap({ milestone, state: milestone ? 'all' : 'open' })
 
   console.log(issuesMap)
 }
