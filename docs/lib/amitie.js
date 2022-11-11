4e93655458877e63bf93476749606d099f15343d
IgorNovozhilov <maildrakon@gmail.com>
2022-11-11 23:05:40 +0300

    amitie-title

--- docs/lib/amitie.js
+++ docs/lib/amitie.js
@@ -5,7 +5,7 @@ import { escapeHTML } from '../lib/utils.js'
  * @returns {string}
  */
 function renderAmitieRow(amitie) {
-  let html = '<div class="amitie-row"><a class="amitie-button amitie-blue text-button" ' +
+  let html = '<div class="amitie-row"><a class="amitie-title amitie-blue text-button" ' +
     `target="_self" href="${amitie.url}">${escapeHTML(amitie.title)}</a>` +
     '<div class="amitie-actions"><div class="amitie-labels">'
 
@@ -15,7 +15,7 @@ function renderAmitieRow(amitie) {
       ` title="${escapeHTML(label.description)}">${escapeHTML(label.name)}</div>`
   }
 
-  html += '</div><div class="amitie-labels">' +
+  html += '</div><div class="amitie-buttons">' +
     `<span class="text-button copy-button" title="${window.location.origin}${amitie.url}">🔗</span>` +
     `<span class="text-button copy-button" title="${amitie.miniСard}">📋</span>` +
     '</div></div></div>'
